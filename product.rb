24a75056d036fa3d808c2dc6f4b7532dba73c586
maryfranks <maryfranks@gmail.com>
2017-07-09 08:53:44 -0400

    adds attribute accessor and initialize methods for product.rb

---
+++ product.rb
@@ -0,0 +1,19 @@
+class Product
+
+  attr_accessor :name, :base_price, :tax_rate
+
+# ======================= Initialize =======================
+
+  def initialize(name, base_price, tax_rate)
+    @name        = name
+    @base_price  = base_price
+    @tax_rate    = tax_rate
+  end
+
+# ===================== Class Methods ======================
+
+
+
+# ==================== Instance Methods =====================
+
+end
