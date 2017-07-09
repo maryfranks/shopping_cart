7009e8322f31713d7ddb6a611718908363d01258
maryfranks <maryfranks@gmail.com>
2017-07-09 09:42:01 -0400

    adds select product and remove product methods to cart.rb

--- cart.rb
+++ cart.rb
@@ -14,7 +14,21 @@ class Cart
 
   def add_product(name, base_price, tax_rate)
     @products << Product.new(name, base_price, tax_rate)
-    return @products.last 
+    return @products.last
+  end
+
+  def select_product(name)
+    @products.each do |item|
+      if item.name == name
+        return item
+      end
+    end
+  end
+
+  def remove_product(name)
+    unwanted_product = self.select_product(name)
+    @products.delete(unwanted_product)
+    @products
   end
 
 end
